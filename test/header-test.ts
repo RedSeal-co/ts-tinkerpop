a4c9f748f4ad307be015c2e61d90ad9f049bbf13
mfrantz <mfrantz@redsealnetworks.com>
2015-03-28 11:49:49 -0700

    Add header-test to check the ts-pkg-installer output

---
+++ test/header-test.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/ts-tinkerpop/index.d.ts"/>
+/// <reference path="../typings/mocha/mocha.d.ts"/>
+
+describe('header-test', () => {
+  it('compiles', () => {
+    // empty
+  });
+});
