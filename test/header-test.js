8b4fab0738129450646522822b7cae01fa3f804b
mfrantz <mfrantz@redsealnetworks.com>
2015-03-30 13:21:51 -0700

    Change ts-pkg-installer to use bundle.d.ts as main

---
+++ test/header-test.js
@@ -0,0 +1,8 @@
+/// <reference path="../typings/ts-tinkerpop/bundle.d.ts"/>
+/// <reference path="../typings/mocha/mocha.d.ts"/>
+describe('header-test', function () {
+    it('compiles', function () {
+        // empty
+    });
+});
+//# sourceMappingURL=header-test.js.map
--- test/header-test.ts
+++ test/header-test.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/ts-tinkerpop/index.d.ts"/>
+/// <reference path="../typings/ts-tinkerpop/bundle.d.ts"/>
 /// <reference path="../typings/mocha/mocha.d.ts"/>
 
 describe('header-test', () => {
